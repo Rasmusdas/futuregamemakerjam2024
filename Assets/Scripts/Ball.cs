0105c615f9648c1059957d35f3db95a414fa87a7
Rasmusdas <rasmusmkn@gmail.com>
2024-02-02 22:05:54 +0100

    Player can shoot balls :O

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,13 +4,45 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+
+    public bool fired = false;
+    public bool held;
+
+    private Rigidbody _rb;
+
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        
+        if (held)
+        {
+            _rb.velocity = Vector3.zero;
+        }
+    }
+
+    public bool HeldOrFired => fired || held;
+
+
+    public void Shoot(Vector2 dir, float power)
+    {
+        fired = true;
+        held = false;
+        _rb.constraints = RigidbodyConstraints.None;
+
+        _rb.velocity = new Vector3(dir.x, 0, dir.y) * power;
+
+
+    }
+
+
+    public void Pickup()
+    {
+        held = true;
+
+        _rb.constraints = RigidbodyConstraints.FreezeAll;
+
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,70 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public Vector2 movementVector;
+    public Vector2 lookVector;
+    public float speed;
+
+    private CharacterController _controller;
+
+    public Ball heldBall;
+    
     void Start()
     {
-        
+        _controller = GetComponent<CharacterController>();
+        lookVector = Vector2.up;
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        
+        _controller.Move(new Vector3(movementVector.x,-9.82f,movementVector.y) * (speed * Time.deltaTime));
+        transform.LookAt(transform.position+new Vector3(lookVector.x,0,lookVector.y));
+
+        if (heldBall)
+        {
+            heldBall.transform.position = transform.position + new Vector3(lookVector.x,0,lookVector.y)*1.1f;
+            
+        }
+    }
+
+    public void OnMove(InputValue vec)
+    {
+        movementVector = vec.Get<Vector2>();
+    }
+
+    public void OnLook(InputValue vec)
+    {
+        if (vec.Get<Vector2>().magnitude != 0)
+        {
+            lookVector = vec.Get<Vector2>();
+            lookVector.Normalize();
+        }
+    }
+
+    public void OnFire()
+    {
+        if (heldBall)
+        {
+            heldBall.Shoot(lookVector,30f);
+            heldBall = null;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("Ball Hit");
+        if (other.gameObject.TryGetComponent(out Ball ball))
+        {
+            if (!ball.HeldOrFired)
+            {
+                heldBall = ball;
+                ball.Pickup();
+            }
+        }
     }
 }

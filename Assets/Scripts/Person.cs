e6845b6133c1bb0b31efac6696ee60955782d9b4
Rasmusdas <rasmusmkn@gmail.com>
2024-02-03 19:38:10 +0100

    Idk it looks done ot me

--- Assets/Scripts/Person.cs
+++ Assets/Scripts/Person.cs
@@ -8,19 +8,24 @@ public class Person : MonoBehaviour
         { Color.blue, Color.black, Color.cyan, Color.green, Color.red, Color.yellow, Color.magenta };
 
     public int random;
-
+    private PlayerAnimations _anim;
     public Vector3 orgPos;
     void Start()
     {
-        GetComponent<MeshRenderer>().material.color = colors[Random.Range(0, colors.Length)];
+        GetComponentInChildren<SkinnedMeshRenderer>().materials[1].color = colors[Random.Range(0, colors.Length)];
+        _anim = GetComponent<PlayerAnimations>();
         random = Random.Range(0, 100000);
         orgPos = transform.position;
+
+        _anim.Jubii();
+
+        GetComponent<Animator>().speed += Random.value / 5;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = orgPos + new Vector3(0, Mathf.Abs(Mathf.Sin(random + Time.time*5))/1.5f,0);
+        //transform.position = orgPos + new Vector3(0, Mathf.Abs(Mathf.Sin(random + Time.time*5))/1.5f,0);
 
     }
 }
